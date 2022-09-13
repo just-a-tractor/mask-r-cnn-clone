b69a13ba8c51a7500bf4a12e42cebd562142adce
just-a-tractor <45266305+just-a-tractor@users.noreply.github.com>
2022-09-13 12:43:50 +0300

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -151,7 +151,6 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.savefig('foo.png')
     return plt
     
 
