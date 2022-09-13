c6416980f11beaf1d5f07e7bbf4852f58d2ede7a
just-a-tractor <45266305+just-a-tractor@users.noreply.github.com>
2022-09-13 12:21:03 +0300

    Update visualize.py
    return plt instead of showing it in "display_instances"

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -151,7 +151,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.show()
+    return plt
     
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
